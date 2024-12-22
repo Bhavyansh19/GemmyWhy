cc953a7ebf3771a197a3c194db0c526a9955ae37
Bhavyansh Jain <bhavyansh.bj@gmail.com>
2024-12-22 18:28:09 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,35 @@
+const api_key = "";
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyB2qMYGpgSGN6zbnTmZDq3VbttiFGYETEI";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.0-pro",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
